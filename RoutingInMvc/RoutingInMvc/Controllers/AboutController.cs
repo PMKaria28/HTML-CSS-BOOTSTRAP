e3bdeac441cc7a20681ac93ec0342bc56b0b1165
PMKaria28 <165508877+PMKaria28@users.noreply.github.com>
2024-12-31 19:25:49 +0530

    routing in mvc

---
+++ RoutingInMvc/RoutingInMvc/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingInMvc.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index1()
+        {
+            return View();
+        }
+    }
+}
